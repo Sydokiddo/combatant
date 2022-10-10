2a38a71727aef98a134385c9cd0f8f5f2d48565d
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2022-10-10 15:04:23 -0700

    Multiple potions can no longer be inserted into Brewing Stand slots, preventing Potions from being deleted

--- src/main/java/net/sydokiddo/combatant/mixin/items/misc/SlotMixin.java
+++ src/main/java/net/sydokiddo/combatant/mixin/items/misc/SlotMixin.java
@@ -1,24 +1,29 @@
 package net.sydokiddo.combatant.mixin.items.misc;
 
+import net.minecraft.world.Container;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.entity.BrewingStandBlockEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Slot.class)
 public class SlotMixin {
 
+    @Shadow @Final public Container container;
+
     @Redirect(method = "safeInsert(Lnet/minecraft/world/item/ItemStack;I)Lnet/minecraft/world/item/ItemStack;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/Slot;getMaxStackSize(Lnet/minecraft/world/item/ItemStack;)I"))
     private int safeInsert(Slot slot, ItemStack stack) {
-        if (stack.getItem() instanceof PotionItem) {
-            return 8;
+        if (container instanceof BrewingStandBlockEntity) {
+            if (stack.getItem() instanceof PotionItem) {
+                return 1;
+            }
         }
         if (stack.getItem() instanceof SnowballItem || stack.getItem() instanceof EggItem) {
             return 64;
-        }
-        if (stack.getItem() instanceof EnchantedBookItem) {
-            return 8;
         }
         if (stack.getItem() instanceof MinecartItem || stack.getItem() instanceof BoatItem) {
             return 16;
--- src/main/java/net/sydokiddo/combatant/mixin/items/potions/BrewingStandScreenHandlerMixin.java
+++ src/main/java/net/sydokiddo/combatant/mixin/items/potions/BrewingStandScreenHandlerMixin.java
@@ -19,11 +19,7 @@ public abstract class BrewingStandScreenHandlerMixin extends AbstractContainerMe
         super(type, syncId);
     }
 
-    @Inject(method = "quickMoveStack",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/world/inventory/Slot;set(Lnet/minecraft/world/item/ItemStack;)V"),
-            locals = LocalCapture.CAPTURE_FAILSOFT,
-            cancellable = true)
+    @Inject(method = "quickMoveStack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/Slot;set(Lnet/minecraft/world/item/ItemStack;)V"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
     private void onTransferSlot(Player player, int index, CallbackInfoReturnable<ItemStack> info,
                                 ItemStack itemStack, Slot slot, ItemStack itemStack2) {
         // Prevents more than 1 potion from fitting in a slot in a Brewing Stand
@@ -32,8 +28,8 @@ public abstract class BrewingStandScreenHandlerMixin extends AbstractContainerMe
             for (int i = 0; i < 3; i++) {
                 Slot slot2 = this.getSlot(i);
                 if (slot2.getItem().isEmpty() && slot.mayPlace(itemStack2)) {
-                    if (itemStack2.getCount() > 1) {
-                        slot2.set(itemStack2.split(1));
+                    if (itemStack2.getCount() > slot2.getMaxStackSize()) {
+                        slot2.set(itemStack2.split(slot2.getMaxStackSize()));
                     } else {
                         slot2.set(itemStack2.split(itemStack2.getCount()));
                     }
@@ -48,9 +44,7 @@ public abstract class BrewingStandScreenHandlerMixin extends AbstractContainerMe
         }
     }
 
-    @Redirect(method = "quickMoveStack",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/world/inventory/BrewingStandMenu;moveItemStackTo(Lnet/minecraft/world/item/ItemStack;IIZ)Z"))
+    @Redirect(method = "quickMoveStack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/BrewingStandMenu;moveItemStackTo(Lnet/minecraft/world/item/ItemStack;IIZ)Z"))
     private boolean onTransferSlotRedirect(BrewingStandMenu instance, ItemStack itemStack, int i, int a, boolean b) {
         return false;
     }
